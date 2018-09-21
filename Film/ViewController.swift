32417d7be4181ac29c70dfa49e25c29d4f95d3cc
Minh Hoàng Dương <d.minhhoang26@gmail.com>
2018-09-21 18:39:17 +0700

    Initial Commit

---
+++ Film/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Film
+//
+//  Created by Dương Hoàng on 9/21/18.
+//  Copyright © 2018 henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
