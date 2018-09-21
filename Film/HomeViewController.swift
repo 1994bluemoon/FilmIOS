111aad52b41d13ce47312537977ca47ad7849100
Minh Hoàng Dương <d.minhhoang26@gmail.com>
2018-09-22 00:07:52 +0700

    init

---
+++ Film/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Film
+//
+//  Created by Dương Hoàng on 9/21/18.
+//  Copyright © 2018 henry. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbDes: UILabel!
+    @IBOutlet weak var imgLogo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func `switch`(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
